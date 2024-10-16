dd7b45c23f4b626aee115f777045d1f21bba8a09
SAKSHI ROKADE <142677644+sakshirokade@users.noreply.github.com>
2024-10-16 14:06:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Vishwajeet Shinde S8 42470</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
